0d7634fbe01a79feadf9b9209b0b20738e6a1326
Sa123kiko <skk70437@gmail.com>
2019-04-13 07:02:59 +0900

    varidates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
+    if @book.save
     redirect_to @book, notice: "書籍を登録しました。"
+    else
+    render :new
+    end
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
